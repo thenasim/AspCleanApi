7a30cb3e417709045d7d4188d0f5563046612b40
Nasim Uddin <23162353+thenasim@users.noreply.github.com>
2022-09-26 00:32:28 +0600

    Auditable and Product entity

---
+++ src/Domain/Entities/Product.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+
+public class Product : BaseAuditableEntity
+{
+    public string Name { get; set; } = null!;
+    public int Quantity { get; set; }
+}
