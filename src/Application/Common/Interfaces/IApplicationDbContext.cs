bc08db7692a9e52423b275b57f2ac00dfe67643c
Nasim Uddin <23162353+thenasim@users.noreply.github.com>
2022-09-28 17:09:53 +0600

    Change tracking in Application Db Context

--- src/Application/Common/Interfaces/IApplicationDbContext.cs
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Application.Common.Interfaces;
 
 public interface IApplicationDbContext
 {
     DbSet<Product> Products { get; }
+    ChangeTracker ChangeTracker { get; }
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
