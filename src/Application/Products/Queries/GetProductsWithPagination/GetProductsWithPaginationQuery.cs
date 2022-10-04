793fe4f6ae1aab41467e8aa0e658d24f3d55120a
Nasim Uddin <23162353+thenasim@users.noreply.github.com>
2022-10-04 12:57:52 +0600

    Default value

--- src/Application/Products/Queries/GetProductsWithPagination/GetProductsWithPaginationQuery.cs
+++ src/Application/Products/Queries/GetProductsWithPagination/GetProductsWithPaginationQuery.cs
@@ -1,10 +1,14 @@
-﻿using Domain.Entities;
+﻿using System.ComponentModel;
+using Domain.Entities;
 using MediatR;
 
 namespace Application.Products.Queries.GetProductsWithPagination;
 
 public class GetProductsWithPaginationQuery : IRequest<List<Product>>
 {
+    [DefaultValue(1)]
     public int PageNumber { get; set; } = 1;
+
+    [DefaultValue(10)]
     public int PageSize { get; set; } = 10;
 }
